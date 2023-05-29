2ed7562985ab3bca96ec3c2f2fbe0ae77847f233
Tuan Vuong <phamtuanvuong2k@gmail.com>
2023-05-29 10:01:39 +0700

    Add libraries in project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App";
 import "./asset/style/index.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchInterval: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
-  <App />
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
   //</React.StrictMode>
 );
 
