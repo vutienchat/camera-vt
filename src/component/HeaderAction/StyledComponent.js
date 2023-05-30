90ce2d12e4448d4c662673313abc205bdf72dd5a
hungvu-lifesup <vuviethung3498@gmail.com>
2023-05-30 09:20:42 +0700

    header

--- src/component/HeaderAction/StyledComponent.js
+++ src/component/HeaderAction/StyledComponent.js
@@ -8,8 +8,14 @@ export const StyledComponent = styled.div`
     font-family: Sarabun;
     font-size: 16px;
     font-weight: 500;
+    .field-name {
+      font-family: Sarabun;
+      font-size: 16px;
+      font-weight: bold;
+      color: #000;
+    }
     .MuiOutlinedInput-input {
-      padding: 0 14px;
+      padding: 0 14px 0 0;
       font-size: 14px;
     }
     .MuiOutlinedInput-adornedStart {
@@ -17,6 +23,86 @@ export const StyledComponent = styled.div`
     }
     .MuiTextField-root {
       width: 400px;
+    }
+    .MuiOutlinedInput-root {
+      &:hover {
+        border: unset;
+      }
+      .MuiOutlinedInput-notchedOutline {
+        border: 1px solid #d3d3d3;
+      }
+    }
+    .item-field-title {
+      font-weight: bold;
+    }
+  }
+  .select-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+  }
+
+  .MuiSelect-select {
+    height: 48px;
+    min-width: 200px;
+  }
+
+  //custom select
+  .select-wrapper {
+    width: 200px;
+    border: 1px solid #d3d3d3;
+    padding: 0 14px 0 20px;
+    cursor: pointer;
+    border-radius: 4px;
+    position: relative;
+    height: 48px;
+  }
+
+  .custom-select {
+    position: relative;
+  }
+  .select-box {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    .type-value {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+  }
+
+  .option-box {
+    position: absolute;
+    width: calc(100% - 20px);
+    height: 300px;
+    top: calc(100% + 10px);
+    left: 0;
+    background-color: #fff;
+    border: 1px solid #d3d3d3;
+    padding: 10px;
+    .MuiTextField-root {
+      width: 100%;
+    }
+    .search {
+      margin-bottom: 4px;
+    }
+    .list {
+      padding: 0;
+      flex-wrap: wrap;
+      height: 250px;
+      overflow: auto;
+      .list-item {
+        padding: 0 16px;
+        width: 100%;
+        justify-content: space-between;
+        .item {
+          justify-content: space-between;
+          width: 100%;
+          margin: 0;
+        }
+      }
     }
   }
 `;
--- src/component/HeaderAction/index.js
+++ src/component/HeaderAction/index.js
@@ -1,10 +1,69 @@
-import { Box, InputAdornment, TextField } from "@material-ui/core";
+import {
+  Box,
+  Checkbox,
+  ClickAwayListener,
+  FormControlLabel,
+  InputAdornment,
+  List,
+  ListItem,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
 import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { StyledComponent } from "./StyledComponent";
+
 export const HeaderAction = () => {
-  const handleSubmit = (data) => {
-    console.log("data", data);
+  const [open, setOpen] = useState(false);
+
+  const [listCustomerType, setListCustomerType] = useState([]);
+  const [listCustomerTypeValue, setListCustomerTypeValue] = useState([]);
+  const [dataSend, setDataSend] = useState({});
+
+  const getCustomerType = async () => {
+    const res = await axios.get("http://localhost:3004/customerType");
+    if (res.status === 200) {
+      setListCustomerType(res.data);
+    }
   };
+
+  useEffect(() => {
+    getCustomerType();
+  }, []);
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleClickAway = () => {
+    setOpen(false);
+  };
+
+  const handleCheckboxChange = (e) => {
+    const name = e.target.name;
+    const checked = e.target.checked;
+    const value = e.target.value;
+    if (listCustomerTypeValue.includes(name) && !checked) {
+      setListCustomerTypeValue(listCustomerTypeValue.filter((t) => t != name));
+      const newDataSend = { ...dataSend };
+      delete newDataSend[newDataSend.name];
+      setDataSend({ ...newDataSend });
+    }
+    if (!listCustomerTypeValue.includes(name) && checked) {
+      setListCustomerTypeValue([...listCustomerTypeValue, name]);
+      handeChangeSubmit(e);
+    }
+  };
+
+  const handeChangeSubmit = (e) => {
+    // console.log({ name: e.target.name, value: e.target.value });
+    setDataSend({ ...dataSend, [e.target.name]: e.target.value });
+  };
+
+  console.log(dataSend);
   return (
     <StyledComponent>
       <Box
@@ -23,6 +82,7 @@ export const HeaderAction = () => {
             id="input-with-icon-textfield"
             placeholder="Search by Customer ID, Customer Name, Address"
             variant="outlined"
+            name="keyword"
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
@@ -30,8 +90,93 @@ export const HeaderAction = () => {
                 </InputAdornment>
               ),
             }}
+            onChange={handeChangeSubmit}
           />
         </Box>
+        <Box
+          className="item-field"
+          style={{ display: "flex", alignItems: "center", gap: 16 }}
+        >
+          <Typography className="item-field-title">Customer Type</Typography>
+          <Box className="select-wrapper">
+            <ClickAwayListener
+              onClickAway={handleClickAway}
+              className="custom-select"
+            >
+              <Box className="select-box">
+                <Box className="select-box" onClick={handleClick}>
+                  <Box className="type-value">
+                    {listCustomerTypeValue.length === 0
+                      ? "Customer Type"
+                      : listCustomerTypeValue.map((t) => t).toString()}
+                  </Box>
+                  <Box className="">
+                    {open ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+                  </Box>
+                </Box>
+                {open ? (
+                  <Box className="option-box">
+                    <Box className="search">
+                      <TextField
+                        id="input-with-icon-textfield"
+                        placeholder="Search"
+                        variant="outlined"
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <SearchIcon />
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                    </Box>
+                    <List
+                      component="nav"
+                      className="list"
+                      aria-label="secondary mailbox folders"
+                    >
+                      <ListItem button className="list-item">
+                        <FormControlLabel
+                          value="all"
+                          control={
+                            <Checkbox
+                              color="primary"
+                              name="all"
+                              onChange={handleCheckboxChange}
+                            />
+                          }
+                          label="All type"
+                          labelPlacement="start"
+                          className="item"
+                        />
+                      </ListItem>
+                      {listCustomerType?.map((t, i) => {
+                        return (
+                          <ListItem button className="list-item">
+                            <FormControlLabel
+                              value={t.value}
+                              control={
+                                <Checkbox
+                                  color="primary"
+                                  name={t.name}
+                                  onChange={handleCheckboxChange}
+                                />
+                              }
+                              label={t.name}
+                              labelPlacement="start"
+                              className="item"
+                            />
+                          </ListItem>
+                        );
+                      })}
+                    </List>
+                  </Box>
+                ) : null}
+              </Box>
+            </ClickAwayListener>
+          </Box>
+        </Box>
+
         <Box className="item-field"></Box>
       </Box>
     </StyledComponent>
