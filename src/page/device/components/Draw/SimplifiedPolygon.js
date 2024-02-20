d7a55b46e506981518fbca23452a2c26c3281d9c
Nguyễn Doãn Hiếu <hieu.doan@lifesup.local>
2024-02-20 18:06:37 +0700

    update change size + move line

--- src/page/device/components/Draw/SimplifiedPolygon.js
+++ src/page/device/components/Draw/SimplifiedPolygon.js
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Stage, Layer, Line, Ellipse } from "react-konva";
 import simplify from "simplify-js";
 import _ from "lodash";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
 const DrawingComponent = () => {
-  const [lines, setLines] = useState([]);
+  const imageRef = useRef(null);
+  const stageRef = useRef(null);
+  const [isUpdatePoint, setIsUpdatePoint] = useState(false);
   const [isDrawing, setIsDrawing] = useState(false);
-  const [selectedLine, setSelectedLine] = useState(null);
-  const [resizing, setResizing] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
-  const [lastDragPosition, setLastDragPosition] = useState({ x: 0, y: 0 });
+  const [lines, setLines] = useState([]);
   const [filledAreaPoints, setFilledAreaPoints] = useState([]);
-  const [finalPosition, setFinalPosition] = useState({ x: 0, y: 0 });
+  const [lastDragPosition, setLastDragPosition] = useState({ x: 0, y: 0 });
+  const [selectedLine, setSelectedLine] = useState(null);
+  const [pointIndex, setPointIndex] = useState(null);
 
   const handleMouseDown = (e, selectedLine) => {
     if (selectedLine && isDragging) return;
@@ -23,21 +26,30 @@ const DrawingComponent = () => {
   };
 
   const handleMouseMove = (e) => {
-    if (!isDrawing || resizing) return;
-    if (selectedLine == null && !isDragging) {
-      const stage = e.target.getStage();
-      const { x, y } = stage.getPointerPosition();
-      const temptLine = _.cloneDeep(lines);
+    // if (!isDrawing) return;
+    const stage = e.target.getStage();
+    const { x, y } = stage.getPointerPosition();
+    const temptLine = _.cloneDeep(lines);
+    if (selectedLine == null && !isDragging && isDrawing) {
       let lastLine = temptLine[temptLine.length - 1];
       lastLine.points.push({ x, y });
       temptLine.splice(temptLine.length - 1, 1, lastLine);
-      setLines([...temptLine]);
+      setLines(temptLine);
+    }
+    if (isUpdatePoint && pointIndex !== null && selectedLine !== null) {
+      const tempAreaPoints = _.cloneDeep(filledAreaPoints);
+      tempAreaPoints[pointIndex] = { x, y };
+      temptLine[selectedLine].points = tempAreaPoints;
+      setFilledAreaPoints(tempAreaPoints);
+      setLines(temptLine);
     }
   };
 
   const handleMouseUp = (e) => {
     setIsDrawing(false);
     setIsDragging(false);
+    setIsUpdatePoint(false);
+    setPointIndex(null);
     // setSelectedLine(null);
     // Automatically close the last line by connecting its last point to its first point
     if (lines.length > 0) {
@@ -55,17 +67,12 @@ const DrawingComponent = () => {
     setFilledAreaPoints([...lines[index].points]);
   };
 
-  const handleLineDblClick = () => {
-    setResizing(true);
-  };
-
-  const handleStageMouseUp = () => {
-    setResizing(false);
-  };
-
   const handleStageClick = (e) => {
-    if (e.target === e.target.getStage()) {
+    const clickedOnEmpty = e.target === e.target.getStage();
+    console.log("clickedOnEmpty", clickedOnEmpty);
+    if (clickedOnEmpty) {
       setSelectedLine(null);
+    } else {
     }
   };
 
@@ -84,86 +91,105 @@ const DrawingComponent = () => {
       y: point.y + deltaY,
     }));
 
-    console.log("deltaX1", deltaX);
-    console.log("newFilledAreaPoints", newFilledAreaPoints);
-    setFinalPosition({ x: deltaX, y: deltaY });
     setLastDragPosition({ x, y });
-    console.log("filledAreaPoints123123", filledAreaPoints);
-
     setFilledAreaPoints(newFilledAreaPoints);
   };
 
-  const handleLineDragMoveEnd = (index, event) => {
+  const handleLineDragMoveEnd = (index, event, filledAreaPoints) => {
     const newLines = _.cloneDeep([...lines]);
-    // const { x, y } = event.target.position();
-    // const deltaX = x - lastDragPosition.x;
-    // const deltaY = y - lastDragPosition.y;
-    // const lineIndex = [...newLines[index].points].map((point) => ({
-    //   x: point.x + deltaX,
-    //   y: point.y + deltaY,
-    // }));
-
     newLines[index].points = filledAreaPoints;
-    console.log("newLines[index]", newLines[index].points);
-
-    console.log("filledAreaPoints", filledAreaPoints);
-    console.log("newLines", newLines);
     setLines(newLines);
+    setLastDragPosition({ x: 0, y: 0 });
   };
 
-  console.log("lineeeee", lines);
-
-  // useEffect(() => {
-  //   if (selectedLine === null) return;
-  //   setFilledAreaPoints([...lines[selectedLine].points]);
-  // }, [selectedLine, lines]);
-  // console.log("filledAreaPoints", filledAreaPoints);
+  const onClickPoint = (e, index) => {
+    if (selectedLine == null) return;
+    setIsDrawing(false);
+    setIsDragging(false);
+    setPointIndex(index);
+    setIsUpdatePoint(true);
+  };
 
   return (
-    <Stage
-      width={905}
-      height={500}
-      onMouseDown={(e) => handleMouseDown(e, selectedLine)}
-      onMousemove={handleMouseMove}
-      onMouseup={handleMouseUp}
-      onTouchStart={(e) => handleMouseDown(e, selectedLine)}
-      onTouchMove={handleMouseMove}
-      onTouchEnd={handleMouseUp}
-      onClick={handleStageClick}
-      style={{ border: "solid 1px ", width: "100%", height: "100%" }}
+    <TransformWrapper
+      doubleClick={{
+        disabled: true,
+      }}
+      panning={{
+        lockAxisX: true,
+        lockAxisY: true,
+      }}
     >
-      <Layer>
-        {lines.map((line, i) => (
-          <Line
-            key={i}
-            points={[...line.points].flatMap((point) => [point.x, point.y])}
-            stroke="black"
-            strokeWidth={selectedLine === i ? 1 : 0.5}
-            onClick={() => handleLineClick(i)}
-            onDblClick={handleLineDblClick}
-            fill="rgba(221, 61, 75, 0.50)"
-            onMouseDown={() => handleLineClick(i)}
-            closed={!isDrawing}
-            draggable
-            onDragStart={(event) => handleLineDragStart(i, event)}
-            onDragMove={(event) => handleLineDragMove(i, event)}
-            onDragEnd={(e) => handleLineDragMoveEnd(i, e)}
-          />
-        ))}
+      <TransformComponent>
+        <div
+          // onContextMenu={(e) => e.preventDefault()}
+          // onDoubleClick={(e) => handleOnDoubleClick(e)}
+          ref={imageRef}
+          style={{
+            backgroundImage:
+              "url('https://cdn.vjshop.vn/tin-tuc/5-cach-cai-thien-bo-cuc-anh-phong-canh-thong-qua-phoi-sang-lau/cai-thien-anh-phong-canh-bang-phuong-phap-chup-phoi-sang-lau.jpg')",
+            height: "100%",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            width: "100%",
+            minWidth: 900,
+            minHeight: 500,
+          }}
+        >
+          <Stage
+            width={900}
+            height={500}
+            onMouseDown={(e) => handleMouseDown(e, selectedLine)}
+            onMousemove={handleMouseMove}
+            onMouseup={handleMouseUp}
+            onTouchStart={(e) => handleMouseDown(e, selectedLine)}
+            onTouchMove={handleMouseMove}
+            onTouchEnd={handleMouseUp}
+            onClick={handleStageClick}
+            ref={stageRef}
+            style={{ border: "solid 1px ", width: "100%", height: "100%" }}
+          >
+            <Layer>
+              {lines.map((line, i) => (
+                <Line
+                  key={i}
+                  points={[...line.points].flatMap((point) => [
+                    point.x,
+                    point.y,
+                  ])}
+                  stroke="black"
+                  strokeWidth={selectedLine === i ? 1 : 0.5}
+                  onClick={() => handleLineClick(i)}
+                  fill="rgba(221, 61, 75, 0.50)"
+                  onMouseDown={() => handleLineClick(i)}
+                  closed={!isDrawing}
+                  draggable
+                  onDragStart={(event) => handleLineDragStart(i, event)}
+                  onDragMove={(event) => handleLineDragMove(i, event)}
+                  onDragEnd={(e) =>
+                    handleLineDragMoveEnd(i, e, filledAreaPoints)
+                  }
+                />
+              ))}
 
-        {selectedLine !== null &&
-          filledAreaPoints.map((point, index) => (
-            <Ellipse
-              key={index}
-              x={point.x}
-              y={point.y}
-              radiusX={5}
-              radiusY={5}
-              fill="red"
-            />
-          ))}
-      </Layer>
-    </Stage>
+              {selectedLine !== null &&
+                filledAreaPoints.map((point, index) => (
+                  <Ellipse
+                    key={index}
+                    x={point.x}
+                    y={point.y}
+                    radiusX={5}
+                    radiusY={5}
+                    fill="red"
+                    onMouseDown={(e) => onClickPoint(e, index)}
+                  />
+                ))}
+            </Layer>
+          </Stage>
+        </div>
+      </TransformComponent>
+    </TransformWrapper>
   );
 };
 
--- src/page/device/components/Draw/index.js
+++ src/page/device/components/Draw/index.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Ellipse, Layer, Line, Stage } from "react-konva";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 const DrawCanvas = ({ initialData, onChange }, ref) => {
   const imageRef = useRef(null);
@@ -23,57 +24,61 @@ const DrawCanvas = ({ initialData, onChange }, ref) => {
   };
 
   return (
-    <div
-      onContextMenu={(e) => e.preventDefault()}
-      onDoubleClick={(e) => handleOnDoubleClick(e)}
-      ref={imageRef}
-      style={{
-        backgroundImage:
-          "url('https://media.istockphoto.com/id/481045024/vi/anh/tran-quoc-pagoda.jpg?s=2048x2048&w=is&k=20&c=7AJXcmayI-XfJDOK_jiKWFAVq6D-1UN4ZYrAfaCa5Oc=')",
-        height: "100%",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        width: "100%",
-        minWidth: 845,
-        minHeight: 445,
-      }}
-    >
-      <Stage
-        onClick={handleStageClick}
-        width={imageRef.current ? imageRef.current.clientWidth : 500}
-        height={imageRef.current ? imageRef.current.clientHeight : 500}
-      >
-        <Layer>
-          {lines.map((it, indx) => (
-            <Line
-              points={it.flatMap((point) => [point.x, point.y])}
-              closed
-              // stroke="black"
-              fill="rgba(221, 61, 75, 0.50)"
-              key={indx}
-            />
-          ))}
-          {points.length > 1 && (
-            <Line
-              points={points.flatMap((point) => [point.x, point.y])}
-              closed={false}
-              stroke="black"
-            />
-          )}
-          {filledAreaPoints.map((point, index) => (
-            <Ellipse
-              key={index}
-              x={point.x}
-              y={point.y}
-              radiusX={5}
-              radiusY={5}
-              fill="red"
-            />
-          ))}
-        </Layer>
-      </Stage>
-    </div>
+    <TransformWrapper>
+      <TransformComponent>
+        <div
+          onContextMenu={(e) => e.preventDefault()}
+          onDoubleClick={(e) => handleOnDoubleClick(e)}
+          ref={imageRef}
+          style={{
+            backgroundImage:
+              "url('https://media.istockphoto.com/id/481045024/vi/anh/tran-quoc-pagoda.jpg?s=2048x2048&w=is&k=20&c=7AJXcmayI-XfJDOK_jiKWFAVq6D-1UN4ZYrAfaCa5Oc=')",
+            height: "100%",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            width: "100%",
+            minWidth: 845,
+            minHeight: 445,
+          }}
+        >
+          <Stage
+            onClick={handleStageClick}
+            width={imageRef.current ? imageRef.current.clientWidth : 500}
+            height={imageRef.current ? imageRef.current.clientHeight : 500}
+          >
+            <Layer>
+              {lines.map((it, indx) => (
+                <Line
+                  points={it.flatMap((point) => [point.x, point.y])}
+                  closed
+                  // stroke="black"
+                  fill="rgba(221, 61, 75, 0.50)"
+                  key={indx}
+                />
+              ))}
+              {points.length > 1 && (
+                <Line
+                  points={points.flatMap((point) => [point.x, point.y])}
+                  closed={false}
+                  stroke="black"
+                />
+              )}
+              {filledAreaPoints.map((point, index) => (
+                <Ellipse
+                  key={index}
+                  x={point.x}
+                  y={point.y}
+                  radiusX={5}
+                  radiusY={5}
+                  fill="red"
+                />
+              ))}
+            </Layer>
+          </Stage>
+        </div>
+      </TransformComponent>
+    </TransformWrapper>
   );
 };
 
